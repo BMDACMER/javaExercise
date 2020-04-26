a75f402c78093afcd67a7a003e3e80be16e2e018
BMDACMER <1163753605@qq.com>
2020-04-26 23:58:13 +0800

    Leetcode  Test10

--- coding-interviews/src/lintcode/offer/Test10.java
+++ coding-interviews/src/lintcode/offer/Test10.java
@@ -14,7 +14,7 @@ public class Test10 {
 
         boolean[][] visit = new boolean[board.length][board[0].length];
         for (int i = 0; i < board.length; i++) {
-            for (int j = 0; j < board.length; j++) {
+            for (int j = 0; j < board[i].length; j++) {
                 if (solve(board, word, i, j, visit, 0)) {
                     return true;
                 }
