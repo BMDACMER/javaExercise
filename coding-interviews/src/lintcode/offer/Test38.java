891c3788373ba691e3499614bb657c03c4c3c303
BMDACMER <1163753605@qq.com>
2020-06-26 23:51:43 +0800

    18.删除链表中重复的结点

--- coding-interviews/src/lintcode/offer/Test38.java
+++ coding-interviews/src/lintcode/offer/Test38.java
@@ -1,19 +1,30 @@
 package lintcode.offer;
 
 // 18.2   删除链表中重复节点
+// https://www.nowcoder.com/practice/fc533c45b73a41b0b44ccba763f866ef?tpId=13&tqId=11209&tPage=1&rp=1&ru=/ta/coding-interviews&qru=/ta/coding-interviews/question-ranking
 public class Test38 {
     public ListNode deleteDuplication(ListNode pHead) {
-        if (pHead == null || pHead.next == null)
-            return pHead;
-        ListNode next = pHead.next;
-        if (pHead.val == next.val) {
-            while (next != null && pHead.val == next.val)
-                next = next.next;
-            return deleteDuplication(next);
-        } else {
-            pHead.next = deleteDuplication(pHead.next);
-            return pHead;
+        if (pHead == null || pHead.next == null) return pHead;
+
+        ListNode head = new ListNode(0);
+        head.next = pHead;
+        ListNode pre = head;
+        ListNode last = head.next;
+
+        while (last != null) {
+            if (last.next != null && last.val == last.next.val) {
+                // 找到不相等的元素
+                while (last.next != null && last.val == last.next.val) {
+                    last = last.next;
+                }
+                pre.next = last.next;
+                last = last.next;
+            } else {
+                pre = pre.next;
+                last = last.next;
+            }
         }
+        return head.next;
     }
-    
+
 }
---
+++ coding-interviews/src/lintcode/offer/Test39.java
@@ -0,0 +1,4 @@
+package lintcode.offer;
+// 19  Õı„t±íß_Ê½Æ¥Åä
+public class Test39 {
+}
