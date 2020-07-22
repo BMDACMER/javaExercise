6cebd58a4c7697aa1f944df2055ef1f5ec80498a
BMDACMER <1163753605@qq.com>
2020-07-22 10:59:51 +0800

    贪心算法 第四题（763）

--- coding-interviews/src/lintcode/greed/PartitionLabels.java
+++ coding-interviews/src/lintcode/greed/PartitionLabels.java
@@ -9,30 +9,36 @@ import java.util.List;
  * @date: 2020/7/21 22:18
  *
  * 763. 划分字母区间
- * https://leetcode-cn.com/problems/partition-labels/solution/hua-fen-zi-mu-qu-jian-by-leetcode/
- * https://leetcode-cn.com/problems/partition-labels/
- * 思想：遍历字符串中每个字符，
- *              如果字符重复截取该段字符串，遍历全程字符串的下一个字符
- *                      如果字符串不重复，接着遍历下一个字符
- *                      否则 遍历zi'fu
+ * 定义数组 last[char] 来表示字符 char 最后一次出现的下标。定义 anchor 和 j 来表示当前区间的首尾。
+ * 如果遇到的字符最后一次出现的位置下标大于 j， 就让 j=last[c] 来拓展当前的区间。
+ * 当遍历到了当前区间的末尾时(即 i==j )，把当前区间加入答案，同时将 start 设为 i+1 去找下一个区间。
+ *
+ * 作者：LeetCode
+ * 链接：https://leetcode-cn.com/problems/partition-labels/solution/hua-fen-zi-mu-qu-jian-by-leetcode/
+ * 来源：力扣（LeetCode）
+ * 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
  */
 
 public class PartitionLabels {
-    public List<Integer> partitionLabels(String S) {
-        int[] last = new int[26];
-        for (int i = 0; i < S.length(); ++i)
-            last[S.charAt(i) - 'a'] = i;
 
+    public List<Integer> partitionLabels(String S) {
+        int[] last = new int[26];   // 用于存放26个字母最后一次出现的位置
+        for (int i = 0; i < S.length(); i++) {
+            last[S.charAt(i) - 'a'] = i;
+        }
+        // 定义 j 和 anchor来表示当前区间的首尾。
         int j = 0, anchor = 0;
-        List<Integer> ans = new ArrayList();
-        for (int i = 0; i < S.length(); ++i) {
-            j = Math.max(j, last[S.charAt(i) - 'a']);
+        List<Integer> list = new ArrayList<>();
+        for (int i = 0; i < S.length(); i++) {
+            // 如果遇到的字符最后一次出现的位置下标大于 j， 就让 j=last[c] 来拓展当前的区间。
+            j = Math.max(j,last[S.charAt(i) - 'a']);
+            // 当遍历到了当前区间的末尾时(即 i==j )，把当前区间加入答案，
+            // 同时将 start 设为 i+1 去找下一个区间。
             if (i == j) {
-                ans.add(i - anchor + 1);
+                list.add(i - anchor + 1);
                 anchor = i + 1;
             }
         }
-        return ans;
+        return list;
     }
-
 }
