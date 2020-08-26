3a52af8111a073ce0ea8c5dca5ffd9ca46d0f40b
BMDACMER <1163753605@qq.com>
2020-08-26 23:13:01 +0800

    查找表+对撞指针

--- LeetcodeClassification/src/查找表/快乐数.java
+++ LeetcodeClassification/src/查找表/快乐数.java
@@ -7,6 +7,8 @@ import java.util.HashSet;
  * @email 1163753605@qq.com
  * @date: 2020/8/26 9:18
  * 202  Happy number
+ *
+ * https://leetcode-cn.com/problems/happy-number/
  */
 public class ¿ìÀÖÊı {
     public boolean isHappy(int n) {
