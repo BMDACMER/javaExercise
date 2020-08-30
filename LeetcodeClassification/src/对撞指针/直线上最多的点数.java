ff5a13c62bad4ae56795c31842de8339f56a2b69
BMDACMER <1163753605@qq.com>
2020-08-30 11:31:52 +0800

    对撞指针

--- LeetcodeClassification/src/对撞指针/直线上最多的点数.java
+++ LeetcodeClassification/src/对撞指针/直线上最多的点数.java
@@ -12,11 +12,11 @@ package 对撞指针;
 public class 直线上最多的点数 {
     // 暴力算法
     public int maxPoints(int[][] points) {
-        // 还有就是点的数量只有两个，或者一个，零个的时候，直接返回点的数量即可。
+        // 1）还有就是点的数量只有两个，或者一个，零个的时候，直接返回点的数量即可。
         if (points.length < 3) {
             return points.length;
         }
-        // 我们还需要考虑所有点都相等的情况，这样就可以看做所有点都在一条直线上。
+        // 2）我们还需要考虑所有点都相等的情况，这样就可以看做所有点都在一条直线上。
         int i = 0;
         for (; i < points.length - 1; i++) {
             if (points[i][0] != points[i + 1][0] || points[i][1] != points[i + 1][1]) {
@@ -27,6 +27,7 @@ public class 直线上最多的点数 {
         if (i == points.length - 1) {
             return points.length;
         }
+
         // 因为我们两点组成一条直线，必须保证这两个点不重合。所以我们进入第三层循环之前，如果两个点相等就可以直接跳过。
         int max = 0;
         for (i = 0; i < points.length; i++) {
@@ -65,6 +66,7 @@ public class 直线上最多的点数 {
     return y22.subtract(y11).multiply(x0.subtract(x22)).equals(y0.subtract(y22).multiply(x22.subtract(x11)));
 }*/
 
+    //判断是否共线 此时采用的是最大公约数，约到最简形式，比较等号两侧分字分母是否相等即可
     private boolean test(int x1, int y1, int x2, int y2, int x, int y) {
         int g1 = gcd(y2 - y1, x2 - x1);
         if(y == y2 && x == x2){
@@ -74,6 +76,7 @@ public class 直线上最多的点数 {
         return (y2 - y1) / g1 == (y - y2) / g2 && (x2 - x1) / g1 == (x - x2) / g2;
     }
 
+    // 求最大公约数
     private int gcd(int a, int b) {
         while (b != 0) {
             int temp = a % b;
