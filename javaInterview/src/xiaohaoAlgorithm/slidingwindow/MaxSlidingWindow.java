aeb62871be37c7b2b402a0a8ec085c1aa3f5a023
BMDACMER <1163753605@qq.com>
2020-12-23 17:52:37 +0800

    leetcode 滑动窗口

---
+++ javaInterview/src/xiaohaoAlgorithm/slidingwindow/MaxSlidingWindow.java
@@ -0,0 +1,53 @@
+package xiaohaoAlgorithm.slidingwindow;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * @author:guohao
+ * @email 1163753605@qq.com
+ * @date: 2020/12/23 17:36
+ */
+public class MaxSlidingWindow {
+    public int[] maxSlidingWindow(int[] nums, int k) {
+        MonotonicQueue window = new MonotonicQueue();
+        List<Integer> list = new ArrayList<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            if (i < k - 1) {
+                window.push(nums[i]);
+            } else {
+                window.push(nums[i]);
+                list.add(window.max());
+                window.pop(nums[i - k + 1]);
+            }
+        }
+
+        int[] arr = new int[list.size()];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = list.get(i);
+        }
+        return arr;
+    }
+}
+
+class MonotonicQueue{
+    private LinkedList<Integer> q = new LinkedList<>();
+
+    public void push(int n) {
+        while (!q.isEmpty() && q.getLast() < n) {
+            q.pollLast();
+        }
+        q.addLast(n);
+    }
+
+    public int max() {
+        return q.getFirst();
+    }
+
+    public void pop(int n) {
+        if (n == q.getFirst())
+            q.pollFirst();
+    }
+}
