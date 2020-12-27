af494aba6d36289ec66ecdecce6b21c38fe6fd8b
BMDACMER <1163753605@qq.com>
2020-12-27 10:19:25 +0800

    leetcode 单词规律 第二种解法

--- javaInterview/src/xiaohaoAlgorithm/oneQuestionPerDay/WordPattern.java
+++ javaInterview/src/xiaohaoAlgorithm/oneQuestionPerDay/WordPattern.java
@@ -36,4 +36,30 @@ public class WordPattern {
         }
         return true;
     }
+
+    // ·½·¨¶þ
+    public boolean wordPattern2(String pattern, String str) {
+        String[] strings = str.split(" ");
+        int n = strings.length;
+        if (n != pattern.length()) return false;
+
+        HashMap<Character, String> p2s = new HashMap<>();
+        HashMap<String, Character> s2p = new HashMap<>();
+        for (int i = 0; i < n; i++) {
+            char p = pattern.charAt(i);
+            String s = strings[i];
+            if (p2s.containsKey(p) && !p2s.get(p).equals(s) || s2p.containsKey(s) && s2p.get(s) != p)
+                return false;
+            p2s.put(p, s);
+            s2p.put(s,p);
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        WordPattern w = new WordPattern();
+        String pattern = "abba";
+        String str = "dog cat cat dog";
+        System.out.println(w.wordPattern2(pattern, str));
+    }
 }
