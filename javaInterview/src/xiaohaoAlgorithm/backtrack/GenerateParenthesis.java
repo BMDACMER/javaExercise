c6c8c78fe362184f775f61829c896042894274e8
BMDACMER <1163753605@qq.com>
2020-12-28 09:51:55 +0800

    leetcode 回溯法 生成括号

--- javaInterview/src/xiaohaoAlgorithm/backtrack/GenerateParenthesis.java
+++ javaInterview/src/xiaohaoAlgorithm/backtrack/GenerateParenthesis.java
@@ -3,8 +3,6 @@ package xiaohaoAlgorithm.backtrack;
 import xiaohaoAlgorithm.剑指offer.ListNode;
 
 import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.LinkedList;
 import java.util.List;
 
 /**
@@ -28,47 +26,70 @@ public class GenerateParenthesis {
         List<String> list = new ArrayList<>();
         if (n == 0) return list;
         // 回溯过程中的途径
-        LinkedList<String> track = new LinkedList<>();
+        StringBuilder track = new StringBuilder();
         // 可用的左括号和右括号数量初始化为n
         backtrack(n,n,track,list);
         return list;
     }
 
 
-    boolean first = true, last = false;
-    private void backtrack(int left, int right, LinkedList<String> track, List<String> res) {
+    private void backtrack(int left, int right, StringBuilder track, List<String> res) {
         if (left < 0 || right < 0) return;
+        // 左括号剩下多了 说明不合法
         if (right < left) return;
+
         if (left == 0 && right == 0) {
-            Iterator<String> it = track.iterator();
-            StringBuilder sb = new StringBuilder();
-            if (first) {
-                sb.append("[");
-                first = false;
-            }
-            sb.append("\"");
-            while (it.hasNext()) {
-                sb.append(it.next());
-            }
-            sb.append("\"").append(",");
-            res.add(sb.toString());
+           res.add(track.toString());
             return;
         }
         // 尝试添加一个左括号
-        track.offerLast("(");
+        track.append("(");
         backtrack(left-1,right,track,res);
-        track.pollLast();
+        track.deleteCharAt(track.length() - 1);
 
         // 尝试添加一个右括号
-        track.offerLast(")");
+        track.append(")");
         backtrack(left,right-1,track,res);
-        track.pollLast();
+        track.deleteCharAt(track.length() - 1);
+    }
+
+    // 方法二： 采用dfs算法
+    /**
+     * 还是满足两条规律
+     *      * 1、左右括号相等
+     *      * 2、对于字串，左括号的数量都要大于等于右括号的数量
+     * @param n
+     * @return
+     */
+    public List<String> generateParenthesis2(int n) {
+        List<String> result = new ArrayList<>();
+        if (n == 0) return result;
+        String track = "";
+        dfs(n,n,track,result);
+        return result;
+    }
+
+    private void dfs(int left, int right, String track, List<String> result) {
+        if (left < 0 || right < 0) return;
+        if (right < left) return;
+        if (left == 0 && right == 0) {
+            result.add(track);
+            return;
+        }
+
+        if (left > 0) {
+            dfs(left-1,right,track+"(",result);
+        }
+        if (right > 0) {
+            dfs(left,right-1,track+")",result);
+        }
+
     }
 
 
     public static void main(String[] args) {
         GenerateParenthesis g = new GenerateParenthesis();
-        List<String> list = g.generateParenthesis(3);
+        List<String> list = g.generateParenthesis2(3);
         for (String s : list) {
             System.out.println(s);
         }
