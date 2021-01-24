b05372b15c945119eed97d44be4764a2a0c117cd
BMDACMER <1163753605@qq.com>
2021-01-24 23:49:32 +0800

    leetcode 脑经急转弯  easy

---
+++ javaInterview/src/guohaoAlgorithm/dynamic/DivisorGame.java
@@ -0,0 +1,16 @@
+package guohaoAlgorithm.dynamic;
+
+/**
+ * @author:guohao
+ * @email 1163753605@qq.com
+ * @date: 2021/1/24 23:24
+ *
+ * 1025. ³ýÊý²©ÞÄ
+ * https://leetcode-cn.com/problems/divisor-game/
+ */
+public class DivisorGame {
+
+    public boolean divisorGame(int N) {
+        return N % 2 == 0;
+    }
+}
