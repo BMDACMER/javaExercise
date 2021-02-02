3b6c9c8a459c0f2e35f9339ee0ee457ba7d815eb
BMDACMER <1163753605@qq.com>
2021-02-02 19:13:33 +0800

    leetcode 链表

---
+++ javaInterview/src/guohaoAlgorithm/list/MiddleNode.java
@@ -0,0 +1,33 @@
+package guohaoAlgorithm.list;
+
+/**
+ * @author:guohao
+ * @email 1163753605@qq.com
+ * @date: 2021/2/2 18:02
+ *
+ * 876. 链表的中间结点   easy难度
+ * 给定一个头结点为 head 的非空单链表，返回链表的中间结点。
+ *
+ * 如果有两个中间结点，则返回第二个中间结点。
+ *
+ *
+ *
+ * 示例 1：
+ *
+ * 输入：[1,2,3,4,5]
+ * 输出：此列表中的结点 3 (序列化形式：[3,4,5])
+ * 返回的结点值为 3 。 (测评系统对该结点序列化表述是 [3,4,5])。
+ * 注意，我们返回了一个 ListNode 类型的对象 ans，这样：
+ * ans.val = 3, ans.next.val = 4, ans.next.next.val = 5, 以及 ans.next.next.next = NULL.
+ */
+public class MiddleNode {
+
+    public ListNode middleNode(ListNode head) {
+        ListNode slow = head, fast = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow;
+    }
+}
