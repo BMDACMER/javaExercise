2040cce8a7c14f94e60ecf9301a0c86e6a8b6026
BMDACMER <1163753605@qq.com>
2021-02-13 10:52:56 +0800

    leetcode 每日一题 easy  448. 找到所有数组中消失的数字 优化了

--- javaInterview/src/guohaoAlgorithm/oneQuestionPerDay/FindDisappearedNumbers.java
+++ javaInterview/src/guohaoAlgorithm/oneQuestionPerDay/FindDisappearedNumbers.java
@@ -25,6 +25,7 @@ import java.util.List;
  */
 public class FindDisappearedNumbers {
 
+    // 时间复杂度和空间复杂度均为 O(n)
     public List<Integer> findDisappearedNumbers(int[] nums) {
         if (nums == null || nums.length == 0) return new ArrayList<>();
         int n = nums.length;
@@ -43,4 +44,37 @@ public class FindDisappearedNumbers {
 
         return res;
     }
+
+    /**
+     * 时间复杂度和空间复杂度均为 O(n)
+     *
+     * 具体来说，遍历 nums，每遇到一个数 x，就让 nums[x?1] 增加 n。
+     * 由于 nums 中所有数均在 [1,n] 中，增加以后，这些数必然大于 n。最后我们遍历
+     * nums，若 nums[i] 未大于 n，就说明没有遇到过数 i+1。这样我们就找到了缺失的数字。
+     *
+     * 注意，当我们遍历到某个位置时，其中的数可能已经被增加过，因此需要对 n取模来还原出它本来的值。
+     *
+     * 作者：LeetCode-Solution
+     * 链接：https://leetcode-cn.com/problems/find-all-numbers-disappeared-in-an-array/solution/zhao-dao-suo-you-shu-zu-zhong-xiao-shi-d-mabl/
+     * 来源：力扣（LeetCode）
+     * 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+     */
+    public List<Integer> findDisappearedNumbersII(int[] nums) {
+        int n = nums.length;
+        int x;
+        for (int i = 0; i < n; i++) {
+            x = (nums[i] - 1) % n;
+            nums[x] += n;
+        }
+
+        List<Integer> res = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            if (nums[i] <= n) {
+                res.add(i + 1);
+            }
+        }
+        return res;
+    }
+
+
 }
